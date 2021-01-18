f289c831b5515287c77a33531ae44c1d0fab0e9e
KimYunHwa <rain3810@gmail.com>
2021-01-18 17:51:40 +0900

    [ADD] Command에 대한 신규 프로젝트 추가

---
+++ WPF.APPLICATION/WPFCommandsDemo/WPFCommandsDemo/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFCommandsDemo
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public Command()
+        {
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
