be26c9fee4cde88469231537103fa236a1c5cc00
KimYunHwa <rain3810@gmail.com>
2021-01-18 15:54:50 +0900

    [EDIT] 이름바꾸기 및 프로젝트 정리

--- WPF.APPLICATION/MVVMSample/WpfApplication1/AdancedModel.cs
+++ WPF.APPLICATION/MVVMSample/WpfApplication1/AdancedModel.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace WpfApplication1
+namespace MVVMSample
 {
     public class Person : INotifyPropertyChanged
     {
--- WPF.APPLICATION/MVVMSample/WpfApplication1/AdvancedWindow.xaml.cs
+++ WPF.APPLICATION/MVVMSample/WpfApplication1/AdvancedWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace WpfApplication1
+namespace MVVMSample
 {
     /// <summary>
     /// Interaction logic for AdvancedWindow.xaml
