286e85e950d9e5f01defcfd98f7e126917f120d3
KimYunHwa <rain3810@gmail.com>
2021-01-28 17:17:11 +0900

    [ADD] Command와 CommandParameter를 이용한 컨트롤 간 속변 변경 연습 샘플

---
+++ WPF.APPLICATION/WPFCommandsDemo3/VIewModel/Command/DelegateCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFCommandsDemo3.VIewModel.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new NullReferenceException("execute can not null");
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+
+        }
+
+        public DelegateCommand(Action<object> execute) : this(execute, null)
+        { }
+
+        //내 컨트롤이 아닌, 남의 컨트롤의 상태를 보고, enable 여부를 결정하려면, 
+        //남의 컨트롤을 변수로 가지고 있든, 해당 이벤트 사용하든 해야한다.
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (this._execute == null) ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+
+
+    }
+}
---
+++ WPF.APPLICATION/WPFCommandsDemo3/VIewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFCommandsDemo3.VIewModel.Command;
+
+namespace WPFCommandsDemo3.VIewModel
+{
+    public class MainViewModel
+    {
+        /// <summary>
+        /// Combobox 용 리스트 컬렉션
+        /// </summary>
+        public ObservableCollection<string> MyMessages { get; private set; } = new ObservableCollection<string>() { "안녕하세요!", "제 이름은 KYH입니다.", "메세지박스", "콘솔" };
+
+        /// <summary>
+        /// Button-MessageBox 용 Command
+        /// </summary>
+        public DelegateCommand MessageBoxCommand { get; private set; }
+
+        /// <summary>
+        /// Button-Consol용 Command
+        /// </summary>
+        public DelegateCommand ConsoleCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            this.MessageBoxCommand = new DelegateCommand(this.DisplayMessageBox, this.MessageBoxCanUse);
+            this.ConsoleCommand = new DelegateCommand(this.DisplayConsole, this.ConsoleCanUse);
+        }
+
+        public void DisplayMessageBox(object message)
+        {
+            MessageBox.Show((string)message);
+        }
+
+        public void DisplayConsole(object message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public bool MessageBoxCanUse(object message)
+        {
+            return !(message?.ToString() == "콘솔");
+        }
+
+        public bool ConsoleCanUse(object message)
+        {
+            return !(message?.ToString() == "메세지박스");
+        }
+    }
+}
