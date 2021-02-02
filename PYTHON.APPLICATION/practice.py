6e8d754cf09e8e438cc5ada280762d1b5e2bdbc5
KimYunHwa <rain3810@gmail.com>
2021-02-02 16:07:08 +0900

    [INIT] 파이선 연습프로젝트

---
+++ PYTHON.APPLICATION/practice.py
@@ -0,0 +1,133 @@
+# print(max(6,5,4,43))
+
+# from math import *
+# print(float(5.77))
+# print(ceil(4.15))
+# print(sqrt(25))
+
+# from random import *
+# print(random())
+# print(random() *10)
+# print(int(random() * 10 ))
+# print(int(random() * 10 ))
+# print(int(random() * 10 ))
+# print(int(random() * 10 + 1))
+# print(int(random() * 10 + 1))
+# print(int(random() * 10 + 1))
+# print(int(random() * 10 + 1))
+# print(int(random() * 10 + 1))
+
+# print(int(random() * 45 ) + 1 )
+# print(int(random() * 45 ) + 1 )
+# print(int(random() * 45 ) + 1 )
+# print(int(random() * 45 ) + 1 )
+# print(int(random() * 45 ) + 1 )
+# print(int(random() * 45 ) + 1 )
+
+# print(randrange(1,46))
+# print(randrange(1,46))
+# print(randrange(1,46))
+# print(randrange(1,46))
+
+# print(randint(1,45))
+# print(randint(1,45))
+# print(randint(1,45))
+# print(randint(1,45))
+
+# from random import *
+# day  = randint(4, 28)
+
+# print("오프라인 스터디 모임 날짜는 매월 ",str( day),'일로 선정되었습니다.')
+
+# sentence = '나는 소년입니다.'
+# print(sentence)
+# sentence2 ="파이썬은 쉬워요"
+# print(sentence2)
+
+# sentence3 = """
+# 나는 소년이고,
+# 파이썬은 쉬워요
+# """
+# print(sentence3)
+
+
+# jumin = "990120-1234567"
+
+# print("성별 :" , jumin[7])
+# print("연 :", jumin[0:2])
+# print("월 :" , jumin[2:4])
+# print("일 :", jumin[4:6])
+
+# print("생년월일 :", jumin[:6])
+# print("생년월일 :", jumin[0:6])
+
+# print("뒤 7자리", jumin[7:])
+# print("뒤 7자리", jumin[7:14])
+# print("뒤 7자리", jumin[-7:])
+
+# pyhton = "Python is Amazing"
+# print(pyhton.lower())
+# print(pyhton.upper())
+# print(pyhton[0].isupper())
+# print(len(pyhton))
+# print(pyhton.replace("Python", "Jave"))
+
+# index = pyhton.index("n")
+# print(index)
+
+# index=pyhton.index("n", index +1)
+# print(index)
+# print(pyhton.find("n"))
+
+# # print(pyhton.index("Java"))
+# print(pyhton.find("Java"))
+# print(pyhton.count("n"))
+
+# print("A" + "B")
+# print("A" ,"B")
+
+# print("나는 %d살입니다." %20)
+# print("나는 %s을 좋아해요." %'파이썬')
+# print("Apple은 %c로 시작해요." %"A")
+
+# print("나는 %s살입니다." %20)
+# print("나는 %s색과 %s색을 좋아해요" %('빨강','파랑'))
+
+# print("나는 {}살입니다.".format(20))
+# print("나는 {}색과 {}색을 좋아해요".format('빨강','파랑'))
+# print("나는 {1}색과 {0}색을 좋아해요".format('빨강','파랑'))
+
+# print("나는 {age}살이며 {color}색을 좋아해요".format(age=20, color="빨강"))
+# print("나는 {age}살이며 {color}색을 좋아해요".format(color="빨강", age=20 ))
+
+# age =20
+# color='빨강'
+# print(f"나는 {age}살이며 {color}색을 좋아해요")
+
+# print("백문이 불여일견\r\n백견이 불여일타")
+# print('저는 "나도코딩" 입니다')
+# print("저는 '나도코딩' 입니다")
+# print("저는 \"나도코딩\" 입니다")
+# print('저는 \'나도코딩\' 입니다')
+
+# print("D:\\Private.Project\\PRACTICE\PYTHON.APPLICATION")
+
+# print('Red apple\rpine')
+
+# print('Redd\bapple')
+
+# print('Red\tApple')
+# print('Red \t Apple')
+
+# siteaddress= "http://naver.com"
+# siteaddress= "http://daum.net"
+# siteaddress= "http://google.com"
+# siteaddress= "http://youtube.com"
+
+# password = siteaddress.replace("http://", "")
+# password= password[:password.index('.')]
+# password = "{}{}{}!".format(password[:3], len(password), password.count('e'))
+# print("생성된 비밀번호 : ", password)
+
+
+
