73c84f91eaf9bdebea0b31e6953e16cf47f2b5ea
KYH-HOME <rain3810@gmail.com>
2021-02-07 16:31:39 +0900

    [EDIT] 연습 (가변까지)

--- PYTHON.APPLICATION/practice.py
+++ PYTHON.APPLICATION/practice.py
@@ -1,3 +1,99 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋ")
+# print("ㅋ"*9)
+
+# print(5>10)
+# print(5<10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not(5>10) )
+
+# '''이렇게 하면 주석이 여러개'''
+# animal = "고양이"
+# age =4
+# name ="해피"
+# hobby = "낮잠"
+# is_adult = age >3
+# print("우리집 " + animal + "의 이름은 " + name+"예요")
+
+# hobby = "공놀이"
+
+# # print(name+"는 " + str(age) + "살이며, "+hobby +"을 아주 좋아해요.")
+# print(name, " 는 " , age, "살이며, ",hobby ,"을 아주 좋아해요.")
+# print( name + "는 어른일까요? " + str(is_adult))
+
+# station = '사당'
+# print(station + '행 열차가 들어오고 있습니다.')
+# station = '신도림'
+# print(station + '행 열차가 들어오고 있습니다.')
+# station = '인청공항'
+# print(station + '행 열차가 들어오고 있습니다.')
+
+# print(1+2)
+# print(2**3)
+# print(5//3)
+# print(5/3)
+# print(5%3)
+# print(10 >3)
+# print (4 >=7)
+# print( 10 <3)
+# print(4 <=4)
+# print(3==3)
+# print(3==2)
+# print(3+4 == 7)
+
+# print(3!=3)
+# print(1 != 3)
+# print(not (1!=1))
+
+# print((3 >0) and (3<5))
+# print((3>0 &(3<5)))
+
+# print((3 > 0) or (3 > 5))
+# print((3 > 0) | (3 > 5))
+
+# print(5>4>3)
+# print(5>4>7)
+
+# print(2+3 *4)
+# print(2+3)
+
+# number= 2 +3 *4
+# print(number)
+# number +=2
+# print(number)
+
+# number *=2
+# print(number)
+# number /=2
+# print(number)
+# number %=5
+# print(number)
+
+# print(abs(-5)) 
+# print (pow(4,2))
+# print(max(5,2,10))
+# print(min(2,3,5))
+# print(round(3.14))
+
+
+# from math import *
+# print(floor(4.99))
+# print(ceil(3.14))
+# print(sqrt(16))
+
+# 3-4 랜덤함수
 # print(max(6,5,4,43))
 
 # from math import *
@@ -318,4 +414,192 @@
 ####################################
 
 #IF###############
+# weather = input('오늘 날씨는 어때요?\r\n')
+# # weather = '맑아요'
+# if weather =="비" or weather=="눈":
+#     print('우산을 챙기세요.')
+# elif weather == '미세먼지':
+#     print('마스트!')
+# else:
+#     print("준비물 필요없어요.")
 
+# temp = int(input('기온은 어때요?\r\n'))
+# if 30 <= temp:
+#     print('너무 더워요. 나가지 마세요')
+# elif 10 <= temp and 30 >temp:
+#     print('괜찮은 날씨예요.')
+# elif 0 <=temp <10: #elif temp >=0  and temp <10:
+#     print ('외투를 챙기세요.')
+# else:
+#     print ('너무 추워요. 나가지 마세요')
+####################################
+
+#FOR###############
+# print('대기번호 : 1')
+# print('대기번호 : 2')
+# print('대기번호 : 3')
+# print('대기번호 : 4')
+
+# for waiting_no in range(1,6):
+#     print('대기번호 : {0}'.format(waiting_no))
+
+# starbuck = ['아이언맨', '토르', '아이엠 그루트']
+# for customer in starbuck:
+#     print('{0}, 커피가 준비되었습니다.'.format(customer))
+####################################
+
+#WHILE###############
+# customer ='토르'
+# index = 5
+
+# while index >=1:
+#     print('{0}, 커피가 준비되었습니다. {1}번 남았어요.'.format(customer,index))
+#     index-=1
+
+#     if index ==0:
+#         print('커피는 폐기처분 되었습니다.')
+
+
+# customer ='아이언맨'
+# index = 1
+
+# while True:
+#     print('{0}, 커피가 준비되었습니다. {1}번 남았어요.'.format(customer,index))
+#     index+=1
+
+
+# customer = '토르'
+# person = 'unknown'
+
+# while person != customer:
+#     print('{0}, 커피가 준비되었습니다.'.format(customer))
+#     person = input('이름이 어떻게 되세요?')
+
+####################################
+
+#continue와 break###############
+# absent =[2,5] 
+# no_book = [7]
+
+# for student in range(1, 11):
+#     if student in absent:
+#         continue
+#     elif  student in no_book:
+#         print('오늘 수업 여기까지. {0}는 교무실로 따라와'.format(student))
+#         break
+#     print('{0}, 책을 읽어봐'.format(student))
+####################################
+
+#한줄 for###############
+#출석번호가 101,102,103....
+# student =[1,2,3,4,5]
+# print(student)
+# student=[i +100 for i in student]
+# print(student)
+
+# student = ['iron name', 'thor', 'i am groot']
+# student=[len(st) for st in student]
+# print(student)
+# student = ['iron name', 'thor', 'i am groot']
+# student = [std.upper() for std in student]
+# print(student)
+
+####################################
+#퀴즈###############
+
+# from random import *
+
+# people = list(range(1,51))
+# myUser = 0
+
+# for user in range(1,51):#people:
+#     time =randint(5, 50)
+    
+#     if 5 <= time <= 15:
+#         myUser +=1
+#         print('[O] {0}번쨰 손님 (소요시간 : {1}분)'.format(user, time))
+#     else:
+#         print('[ ] {0}번쨰 손님 (소요시간 : {1}분)'.format(user, time))
+
+# print('총 탑승 승객 : {0} 분'.format(myUser))
+####################################
+#함수###############
+
+# def open_account():
+#     print('새로운 계좌가 생성되었습니다.')
+
+# open_account()
+####################################
+#전달값과 반환값###############
+
+# def open_account():
+#     print('새로운 계좌가 생성되었습니다.')
+
+# def deposit(balance, money):
+#     print('입금이 완료되었습니다. 잔액은 {0} 원입니다.'.format(balance + money))
+#     return balance + money
+
+# def withdraw (balance, money):
+#     if balance > money:
+#         print('출금이 완료되었습니다. 잔액은 {0} 원입니다.'.format(balance-money))
+#         return balance-money
+#     else:
+#         print('출금이 완료되지 않았습니다. 잔액은 {0} 원입니다.'.format(balance))
+#         return balance
+
+# def withdraw_night(balance, money):
+#     commission= 100
+#     return commission, balance-money -commission
+
+# balance = 0
+# balance = deposit(balance, 1000)
+# # balance = withdraw(balance, 500)
+
+# commission, balance = withdraw_night(balance, 500)
+
+# print('수수료는 {0} 원이며, 잔액은 {1} 원입니다.'.format(commission,balance))
+
+####################################
+#기본값###############
+# def profile(name, age, main_lang):
+#     print('이름은 : {0}\t나이 : {1}\t주 사용언어 : {2}'\
+#         .format(name,age,main_lang ))
+
+# profile('유재석', 20, '파이썬')
+# profile('김태호', 25, '자바')
+
+#같은 동기라면?
+# def profile(name, age=17, main_lang='파이썬'):
+#     print('이름은 : {0}\t나이 : {1}\t주 사용언어 : {2}'\
+#         .format(name,age,main_lang ))
+
+# profile('유재석')
+# profile('김태호')
+
+# ####################################
+# #키워드값###############
+ 
+# def profile(name,age, main_lang):
+#      print(name,age,main_lang)
+
+# profile(name='유재석', age=20, main_lang='파이썬')
+# profile(main_lang='자바', name='김태호', age=25)
+####################################
+#가변인자###############
+# def profile(name, age, lang1,lang2,lang3,lang4,lang5):
+#     print('name : {0}\tage : {1}\t'.format(name,age), end=" ")
+#     print(lang1,lang2,lang3,lang4,lang5)
+
+# def profile2(name, age, *language):
+#     print('name : {0}\tage : {1}\t'.format(name,age), end=" ")
+#     for lang in language:
+#         print(lang, end=' ')
+#     print()
+
+
+# profile('유재석', 20, 'python', 'java', 'c', 'c++', 'C#')
+# profile('김태호', 25, 'kotlin', 'Swift', '', '', '')
+# profile2('유재석', 20, 'python', 'java', 'c', 'c++', 'C#', 'javascript')
+profile2('김태호', 25, 'kotlin', 'Swift')
+####################################
+#지역변수와 전역변수###############
