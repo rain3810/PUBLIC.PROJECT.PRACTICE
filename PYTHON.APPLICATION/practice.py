a1374f6f18e139e9630934489c5e1f39cfe6e3ca
KYH-HOME <rain3810@gmail.com>
2021-02-08 21:24:08 +0900

    [EDIT] 연습

--- PYTHON.APPLICATION/practice.py
+++ PYTHON.APPLICATION/practice.py
@@ -600,6 +600,61 @@
 # profile('유재석', 20, 'python', 'java', 'c', 'c++', 'C#')
 # profile('김태호', 25, 'kotlin', 'Swift', '', '', '')
 # profile2('유재석', 20, 'python', 'java', 'c', 'c++', 'C#', 'javascript')
-profile2('김태호', 25, 'kotlin', 'Swift')
+# profile2('김태호', 25, 'kotlin', 'Swift')
 ####################################
 #지역변수와 전역변수###############
+# gun = 10
+# def checkpoint(soldiers):
+#     global gun #전역 공간에 있는 gun 사용
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+
+# def checkpoint_ret(gun, soldiers):
+#     gun = gun-soldiers
+#     print("[함수 내] 남은 총 : {0}".format(gun))
+#     return gun
+
+
+# print('전체  총 : {0}'.format(gun))
+# # checkpoint(2)
+# gun= checkpoint_ret(gun,2)
+# print('남은  총 : {0}'.format(gun)) 
+####################################
+#퀴즈###############
+# from math import *
+
+# def std_weight(height, gender):
+#     if gender == '여자':
+#         return pow((height /100),2)* 21
+#     else :
+#         return pow((height /100),2) * 22
+
+# height = 175
+# gender = '남자'
+# weigh = std_weight(height, gender)
+
+# print('키 {0}cm {1}의 표준 체중은 {2}kg 입니다.'.format(height, gender, round(weigh,2)))
+####################################
+#표준입출력###############
+# print('python', 'java',  sep=',', end="?")
+# print('무엇이 더 재미있을까요?')
+
+# import sys
+# print('python', 'java', file=sys.stdout)    #표준 출력으로 처리
+# print('python', 'java', file=sys.stderr)    #표준 에러로 처리
+
+# scores ={'math':0, 'Eng':50,'coding':100 }
+# for subject, score in scores.items():
+#     # print(subject, score)
+#     print(subject.ljust(8), str(score).rjust(4), sep=':')
+
+# #은행 대기 순번표 001,002,003
+# for number in range(1, 21):
+#     print('대기번호 :' + str(number).zfill(3))
+
+# answer =input('아무 값이나 입력하세요 : ')
+# answer = 10
+# print(type(answer))
+# print('입력하신 값은 ' + answer + '입니다.')
+####################################
+#다양한 출력 포맷###############
