61167b40f2ec4aa7130f03c90fe03151cc0f0520
KYH-HOME <rain3810@gmail.com>
2021-05-23 17:00:14 +0900

    [EDIT] 컨트롤러 추가

--- ASPNET.APPLICATION/REST_API_TEST/REST_API_TEST/Controllers/Park_Get_Car_InController.cs
+++ ASPNET.APPLICATION/REST_API_TEST/REST_API_TEST/Controllers/Park_Get_Car_InController.cs
@@ -2,41 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-//https://bigenergy.tistory.com/entry/CASPNET-Core-%EC%9B%B9-%EC%9D%91%EC%9A%A9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%A8-REST-API-%EC%84%9C%EB%B2%84-%EB%A7%8C%EB%93%A4%EA%B8%B0-1
 namespace REST_API_TEST.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Produces("application/json")]
+    [Route("api/Park_Get_Car_In")]
+    public class Park_Get_Car_InController : Controller
     {
-        // GET api/values
+        // GET: api/Park_Get_Car_In
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
+        // GET: api/Park_Get_Car_In/5
+        [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
-
-        // POST api/values
+        
+        // POST: api/Park_Get_Car_In
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
-
-        // PUT api/values/5
+        
+        // PUT: api/Park_Get_Car_In/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
-        // DELETE api/values/5
+        
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
