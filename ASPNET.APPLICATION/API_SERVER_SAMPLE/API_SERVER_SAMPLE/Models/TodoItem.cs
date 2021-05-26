0614032edf02e5b9ffba0ad4e622195e6aabc1b7
KYH-ZIN <rain3810@gmail.com>
2021-05-26 17:58:35 +0900

    [ADD] .net 5를 사용한 API Server 프로젝트 신규 생성 (기존 프로젝트 삭제)

---
+++ ASPNET.APPLICATION/API_SERVER_SAMPLE/API_SERVER_SAMPLE/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SERVER_SAMPLE.Models
+{
+    public class TodoItem
+    {
+        /// <summary>
+        /// 고유키
+        /// </summary>
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
