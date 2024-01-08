2253f0cc523aace91246e6b1b5622a4040b33e80
yhkim <yhkim@hanssak.co.kr>
2024-01-08 12:52:37 +0900

    Init Project - GitAciont Public 한 Main 생성

---
+++ GitActionTest_Main/GitActionTest_Main/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitActionTest_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GitActionTest_Main 입니다. 프로그램이 시작 되었습니다.");
+            Console.WriteLine("Library를 호출해보겠습니다.");
+
+            Console.WriteLine("Library가 종료 되었습니다.");
+            Console.WriteLine("이제 Main 저도 물러나 보겠습니다. 안뇽!");
+        }
+    }
+}
