af326fc3052cee1e8c09e2e6cc75b2dadcae33ae
yhkim <yhkim@hanssak.co.kr>
2024-01-15 12:22:23 +0900

    Add Library (GitActiontest_Lib.dll)

--- GitActionTest_Main/GitActionTest_Main/Program.cs
+++ GitActionTest_Main/GitActionTest_Main/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GitActionTest_Lib;
+using System;
 
 namespace GitActionTest_Main
 {
@@ -8,6 +9,10 @@ namespace GitActionTest_Main
         {
             Console.WriteLine("GitActionTest_Main 입니다. 프로그램이 시작 되었습니다.");
             Console.WriteLine("Library를 호출해보겠습니다.");
+
+
+            Introduce lib = new Introduce();
+            lib.Init();
 
             Console.WriteLine("Library가 종료 되었습니다.");
             Console.WriteLine("이제 Main 저도 물러나 보겠습니다. 안뇽!");
